ef779e8c337c73033da1365be2c38b17b7a3a3ff
David Larsson <david.l.larsson.90>
2020-03-11 14:13:39 +0100

    First commit with prime cheking method

---
+++ PrimeNumbers/PrimeNumbers/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace PrimeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Prime();
+            }
+        }
+
+        static void Prime()
+        {
+            Console.WriteLine("Write a number to check if it is a prime number!");
+            int number;
+            bool isInt = int.TryParse(Console.ReadLine(), out number);
+
+
+            if (isInt && number > 1)
+            {
+                if (CheckIfNumberIsPrime(number))
+                {
+                    Console.WriteLine(number + " is a prime number!");
+                }
+                else
+                {
+                    Console.WriteLine(number + " is NOT a prime number!");
+                }
+
+            }
+            else Console.WriteLine("Please enter an integer!");
+        }
+
+        static bool CheckIfNumberIsPrime(int number)
+        {
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
